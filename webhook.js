d3aff40dcc35050d60a375c53fe4f58bdda6a22b
Nikhil Chelani <nikhilchelani.94@gmail.com>
2018-03-16 15:49:11 +0400

    Setup backend to identify and save survey responses

---
+++ webhook.js
@@ -0,0 +1,6 @@
+var localtunnel = require('localtunnel');
+localtunnel(5000, { subdomain: 'dughnahfymgwm' }, function(err, tunnel) {
+  console.log('LT running')
+});
+
+//https://dughnahfymgwm.localtunnel.me/api/surveys/webhooks
